3ddde2b79c5f42e4bdd658d512eb8710dac481fd
paquisha <paquishaxd@gmail.com>
2022-01-31 15:05:37 -0500

    prueba de c#

---
+++ CalculoHoras/CalculoHoras/Program.cs
@@ -0,0 +1,17 @@
+﻿int horasTrabajadas;
+float costoHora;
+float sueldo;
+string linea;
+
+Console.Write("ingresar horas trabajadas: ");
+linea = Console.ReadLine();
+horasTrabajadas = int.Parse(linea);
+
+Console.Write("Ingrese pago por hora: ");
+linea = Console.ReadLine();
+costoHora = int.Parse(linea);
+
+sueldo = horasTrabajadas * costoHora;
+
+Console.Write("sueldo total: " + sueldo);
+Console.ReadKey();
