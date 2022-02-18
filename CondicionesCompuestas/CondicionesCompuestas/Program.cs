25e3e62f2ceb20e3f1d26ee4895ba45f2c2b2f26
paquisha <paquishaxd@gmail.com>
2022-02-18 07:54:46 -0500

    se agrega ejercicio 5 de cuadrantes, seguir agregando los demas ejercicios

--- CondicionesCompuestas/CondicionesCompuestas/Program.cs
+++ CondicionesCompuestas/CondicionesCompuestas/Program.cs
@@ -49,6 +49,8 @@ namespace CondicionesCompuestas
             }
 
             static void primero(){
+                Console.WriteLine("Realizar un programa que pida cargar una fecha cualquiera,"+
+                    "luego verificar si dicha fecha corresponde a Navidad.");
                 int mes, dia;
                 string linea;
                 Console.WriteLine("Ingrese mes: ");
@@ -70,6 +72,8 @@ namespace CondicionesCompuestas
 
             static void segundo()
             {
+                Console.WriteLine("Se ingresan tres valores por teclado, si todos son iguales se imprime la suma del"+
+                    "primero con el segundo y a este resultado se lo multiplica por el tercero.");
                 int num1, num2, num3;
                 string linea;
                 Console.WriteLine("Ingrese primer numero:");
@@ -101,6 +105,9 @@ namespace CondicionesCompuestas
 
             static void tercero()
             {
+                Console.WriteLine("Se ingresan por teclado tres números, si todos los valores"+
+                    "ingresados son menores a 10, imprimir en pantalla la leyenda"+
+                    "Todos los números son menores a diez");
                 int num1, num2, num3;
                 string linea;
                 Console.WriteLine("Ingrese primer numero:");
@@ -125,12 +132,69 @@ namespace CondicionesCompuestas
 
             static void cuarto()
             {
-                Console.WriteLine("cuarto ejercio");
+                Console.WriteLine("Se ingresan por teclado tres números, si al menos uno de los valores ingresados es menor a 10,"+
+                    "imprimir en pantalla la leyenda"+"Alguno de los números es menor a diez");
+                int num1, num2, num3;
+                string linea;
+                Console.WriteLine("Ingrese primer numero: ");
+                linea = Console.ReadLine();
+                num1 = int.Parse(linea);
+                Console.WriteLine("Ingrese segundo numero: ");
+                linea = Console.ReadLine();
+                num2 = int.Parse(linea);
+                Console.WriteLine("Ingrese tercer numero: ");
+                linea = Console.ReadLine();
+                num3 = int.Parse(linea);
+
+                if(num1 < 10 || num2 < 10 || num3 < 10)
+                {
+                    Console.WriteLine("Al menos un numero es menor a 10");
+                }
+                else
+                {
+                    Console.WriteLine("todos son mayores a 10");
+                }
             }
 
             static void quinto()
             {
-                Console.WriteLine("quinto ejercio");
+                Console.WriteLine("Escribir un programa que pida ingresar la coordenada de un punto en el plano,"+
+                    "es decir dos valores enteros x e y (distintos a cero).Posteriormente imprimir en pantalla"+
+                    "en que cuadrante se ubica dicho punto. (1º Cuadrante si x > 0 Y y > 0, 2º Cuadrante: x < 0 Y y > 0, etc.)");
+                int num1, num2;
+                string linea;
+                Console.WriteLine("Ingrese primera c0ordenada: ");
+                linea = Console.ReadLine();
+                num1 = int.Parse(linea);
+                Console.WriteLine("Ingrese segunda coordenada: ");
+                linea = Console.ReadLine();
+                num2 = int.Parse(linea);
+
+                if(num1 > 0 && num2 > 0)
+                {
+                    Console.WriteLine($"x: {num1}, y: {num2}, pertenece al primer cuadrante");
+                }
+                else
+                {
+                    if (num1 < 0 && num2 > 0)
+                    {
+                        Console.WriteLine($"x: {num1}, y: {num2}, pertenece al segundo cuadrante");
+                    }
+                    else
+                    {
+                        if(num1 < 0 && num2 < 0)
+                        {
+                            Console.WriteLine($"x: {num1}, y: {num2}, pertenece a la tercer cuadrante");
+                        }
+                        else
+                        {
+                            if(num1 > 0 && num2 < 0)
+                            {
+                                Console.WriteLine($"x: {num1}, y: {num2}, pertenece al cuarto cuadrante");
+                            }
+                        }
+                    }
+                }
             }
 
             static void sexto()
