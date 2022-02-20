db3f26aa7098b1ba630e96d065b4815ccd34d8b1
paquisha <paquishaxd@gmail.com>
2022-02-20 18:36:10 -0500

    Se limpia de ejercicio mal creado

--- CrudBasico/CrudBasico/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CrudBasico.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
