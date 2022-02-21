7c717232d15ede1f83f1c8553aadc7a95d72ea13
paquisha <paquishaxd@gmail.com>
2022-02-20 21:06:21 -0500

    se agrega configuracion de la primra vista

---
+++ CrudNetCore5/CrudNetCore5/Controllers/LibrosController.cs
@@ -0,0 +1,25 @@
+﻿using CrudNetCore5.Data;
+using CrudNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> listLibros = _context.Libro;
+            return View(listLibros);
+        }
+    }
+}
--- CrudNetCore5/CrudNetCore5/Startup.cs
+++ CrudNetCore5/CrudNetCore5/Startup.cs
@@ -1,6 +1,8 @@
+using CrudNetCore5.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,10 @@ namespace CrudNetCore5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //configuracion de conexion para la db
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
