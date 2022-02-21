1c0a7e57b3cd0deaa1fdbd7db32481eb94e7a150
paquisha <paquishaxd@gmail.com>
2022-02-20 22:06:59 -0500

    se finaliza ejercicio de crud

--- CrudNetCore5/CrudNetCore5/Controllers/LibrosController.cs
+++ CrudNetCore5/CrudNetCore5/Controllers/LibrosController.cs
@@ -16,10 +16,107 @@ namespace CrudNetCore5.Controllers
         {
             _context = context;
         }
+
+        //index
         public IActionResult Index()
         {
             IEnumerable<Libro> listLibros = _context.Libro;
             return View(listLibros);
         }
+
+        //create get create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //create post create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha creado correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //http edit
+        public IActionResult Edit(int? id)
+        {
+
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //obtener el libro
+            var libro = _context.Libro.Find(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
+        }
+
+        //create post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "El libro se ha actualizado correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //http Delete
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //obtener el libro
+            var libro = _context.Libro.Find(id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
+        }
+
+        //create post delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteLibro(int? id)
+        {
+            var libro = _context.Libro.Find(id);
+            if(libro == null)
+            {
+                return NotFound();
+            }
+
+            _context.Libro.Remove(libro);
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "El libro se ha Eliminado correctamente";
+            return RedirectToAction("Index");
+
+        }
+
+
     }
 }
