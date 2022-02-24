0b0ef3553447816695df71a8f8e76e6446a6c571
paquisha <paquishaxd@gmail.com>
2022-02-23 23:02:42 -0500

    se dio la segunda clases

--- pruebasDos/Program.cs
+++ pruebasDos/Program.cs
@@ -1,7 +1,32 @@
 ﻿namespace PruebaDos{
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("cualquier cosa"); 
+           //Confeccionar un programa que lea por teclado tres números distintos y nos muestre el mayor.
+            int num1, num2, num3;
+            string linea;
+
+            Console.WriteLine("Ingrese primer numero: ");
+            linea = Console.ReadLine();
+            num1 = int.Parse(linea);
+
+            Console.WriteLine("Ingrese numero dos: ");
+            linea = Console.ReadLine();
+            num2 = int.Parse(linea);
+
+            Console.WriteLine("Ingrese numero tres: ");
+            linea = Console.ReadLine();
+            num3 = int.Parse(linea);
+
+            if(num1 > num2 && num1 > num3){
+                Console.WriteLine($"{num1} es el mayor");
+            }else{
+                if(num2 > num3){
+                    Console.WriteLine($"{num2} es el mayor");
+                }else{
+                    Console.WriteLine($"{num3} es el mayor");
+                }
+            }
+
         }
     }
 }
