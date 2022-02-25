aa1439996e9a229b9924c8de96efc4c83975fdb3
paquisha <paquishaxd@gmail.com>
2022-02-24 21:34:25 -0500

    se acomoda espacios

--- pruebasDos/Program.cs
+++ pruebasDos/Program.cs
@@ -30,15 +30,6 @@
                 }
             }
 
-
-
-
-
-
-
-
-
-
             //Escribir un programa que pida ingresar la coordenada de un punto en el plano, 
             //es decir dos valores enteros x e y (distintos a cero). Posteriormente imprimir 
             //en pantalla en que cuadrante se ubica dicho punto. (1º Cuadrante si x > 0 Y y > 0 , 2º Cuadrante: x < 0 Y y > 0, etc.)
