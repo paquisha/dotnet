6d260db8905f3d9495a000f38f6165ce7f65c5b6
Luis Andres Grijalva P <gex_grijalval@produbanco.com>
2022-02-26 22:48:51 -0500

    cambios para prueba de while

--- EstructurasRepetitivas/Program.cs
+++ EstructurasRepetitivas/Program.cs
@@ -7,8 +7,16 @@ namespace EstructurasRepetitivas
         static void Main(string[] args)
         {
             int x = 1;
-            while(x <= 10){
-                Console.WriteLine(x);
+            int valor;
+            string linea;
+
+            Console.WriteLine("Ingrese valor: ");
+            linea = Console.ReadLine();
+            valor = int.Parse(linea);
+
+            while(x <= valor){
+                Console.Write(x);
+                Console.Write(" - ");
                 x++;
             }
         }
