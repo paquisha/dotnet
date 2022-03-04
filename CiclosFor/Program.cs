fdfdb83ae352ebe3dfc450d5f6749a5202097903
Luis Andres Grijalva P <gex_grijalval@produbanco.com>
2022-03-03 22:09:38 -0500

    inicio de ejercicios con ciclo for

---
+++ CiclosFor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CiclosFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
