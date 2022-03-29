f896a075f49d61ca66717901bdc883f9efaec9c4
paquisha <paquishaxd@gmail.com>
2022-03-28 22:27:45 -0500

    se agrega primeros passos del sistema

---
+++ SistemaVentas/SistemaVentas/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Areas.Users.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public class InputModel : InputModelRegister
+        {
+            public IFormFile AvatarImage { get; set; }
+            [TempData]
+            public string ErrorMessage { get; set; }
+        }
+    }
+}
