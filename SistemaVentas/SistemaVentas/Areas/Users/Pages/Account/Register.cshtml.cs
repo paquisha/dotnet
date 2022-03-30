88d81ef64bfe57d48a604b3f254ae453c98c4117
paquisha <paquishaxd@gmail.com>
2022-03-30 09:49:46 -0500

    cambios de primera migracion

--- SistemaVentas/SistemaVentas/Areas/Users/Pages/Account/Register.cshtml.cs
+++ SistemaVentas/SistemaVentas/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaVentas.Areas.Users.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
